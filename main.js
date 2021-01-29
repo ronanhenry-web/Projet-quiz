89242269aa7a6c70866993099769fe2b7a7a21c6
henry <henry@SIO.FR>
2021-01-29 11:01:47 +0100

    Itération 1

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 //https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
 //voir quiz prof utilise le meme principe pour importer des donnes pour le JSON
 fetch("quiz.json")
-    .then(function (response) { return response.json(); })
-    .then(function (data) {
-    console.log("le theme est : " + data.theme);
-});
+    .then(function(response) { return response.json(); })
+    .then(function(donne) {
+
+        const theme = document.querySelector(".theme");
+        const questionNumber = document.querySelector(".total-question");
+
+        document.querySelector('.theme').innerHTML = donne.theme;
+        document.querySelector('.total-question').innerHTML = donne.nombreQts;
+
+    });
